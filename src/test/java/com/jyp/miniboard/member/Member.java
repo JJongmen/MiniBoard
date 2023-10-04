51387579117330a2169942a389044ee6430de384
JJongmen <pjy90123@gmail.com>
2023-10-04 14:36:07 +0900

    MemberRepository - 멤버등록

--- src/test/java/com/jyp/miniboard/member/Member.java
+++ src/test/java/com/jyp/miniboard/member/Member.java
@@ -10,5 +10,63 @@ public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private String email;
+    private String password;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String email;
+        private String password;
+
+        Builder id(final Long id) {
+            this.id = id;
+            return this;
+        }
+
+        Builder name(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        Builder email(final String email) {
+            this.email = email;
+            return this;
+        }
+
+        Builder password(final String password) {
+            this.password = password;
+            return this;
+        }
+
+        Member build() {
+            final Member member = new Member();
+            member.id = this.id;
+            member.name = this.name;
+            member.email = this.email;
+            member.password = this.password;
+            return member;
+        }
+    }
 }
--- src/test/java/com/jyp/miniboard/member/MemberRepositoryTest.java
+++ src/test/java/com/jyp/miniboard/member/MemberRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest // JPA 관련된 설정만 로드
@@ -15,5 +16,24 @@ public class MemberRepositoryTest {
     @Test
     void MemberRepository가Null이아님() {
         assertThat(memberRepository).isNotNull();
+    }
+
+    @Test
+    void 멤버등록() {
+        // given
+        final Member member = Member.builder()
+                .name("name")
+                .email("email")
+                .password("password")
+                .build();
+
+        // when
+        final Member result = memberRepository.save(member);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getName()).isEqualTo("name");
+        assertThat(result.getEmail()).isEqualTo("email");
+        assertThat(result.getPassword()).isEqualTo("password");
     }
 }
