037b2cfda1e6158d052952892e13e5cd0ef15a00
JJongmen <pjy90123@gmail.com>
2023-10-08 21:17:12 +0900

    SignControllerTest 리팩터링

--- src/test/java/com/jyp/miniboard/controller/SignControllerTest.java
+++ src/test/java/com/jyp/miniboard/controller/SignControllerTest.java
@@ -6,7 +6,11 @@ import com.jyp.miniboard.dto.sign_up.SignUpRequest;
 import com.jyp.miniboard.exception.MemberErrorResult;
 import com.jyp.miniboard.exception.MemberException;
 import com.jyp.miniboard.service.SignService;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +19,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doThrow;
@@ -38,199 +44,136 @@ class SignControllerTest {
         assertThat(gson).isNotNull();
     }
 
-    @Test
-    void 회원가입실패_name이null임() throws Exception {
-        // given
-        final String url = "/api/v1/sign-up";
-        final SignUpRequest request = new SignUpRequest(null, "name@email.com", "password");
+    @Nested
+    class 회원가입테스트 {
 
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
+        @ParameterizedTest(name = "{1}")
+        @MethodSource("provideInvalidSignUpRequests")
+        void 회원가입실패_잘못된요청값(SignUpRequest request, String description) throws Exception {
+            // given
+            final String url = "/api/v1/sign-up";
 
-        // then
-        resultActions.andExpect(status().isBadRequest());
+            // when
+            final ResultActions resultActions = mockMvc.perform(
+                    MockMvcRequestBuilders.post(url)
+                            .content(gson.toJson(request))
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            // then
+            resultActions.andExpect(status().isBadRequest());
+        }
+
+        private static Stream<Arguments> provideInvalidSignUpRequests() {
+            return Stream.of(
+                    Arguments.of(new SignUpRequest(null, "name@email.com", "password"), "name이 없음"),
+                    Arguments.of(new SignUpRequest("name", null, "password"), "email이 없음"),
+                    Arguments.of(new SignUpRequest("name", "email", null), "password가 없음"),
+                    Arguments.of(new SignUpRequest("name", "wrong email form", "password"), "email 형식이 잘못됨")
+            );
+        }
+
+        @Test
+        void 회원가입실패_이미등록된email임() throws Exception {
+            // given
+            final String url = "/api/v1/sign-up";
+            final SignUpRequest request = new SignUpRequest("name", "name@email.com", "password");
+            doThrow(new MemberException(MemberErrorResult.DUPLICATED_MEMBER_EMAIL)).when(signService).signUp(request);
+
+            // when
+            final ResultActions resultActions = mockMvc.perform(
+                    MockMvcRequestBuilders.post(url)
+                            .content(gson.toJson(request))
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            // then
+            resultActions.andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value(MemberErrorResult.DUPLICATED_MEMBER_EMAIL.name()));
+        }
+
+        @Test
+        void 회원가입성공() throws Exception {
+            // given
+            final String url = "/api/v1/sign-up";
+            final SignUpRequest request = new SignUpRequest("name", "name@email.com", "password");
+
+            // when
+            final ResultActions resultActions = mockMvc.perform(
+                    MockMvcRequestBuilders.post(url)
+                            .content(gson.toJson(request))
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
+
+            // then
+            resultActions.andExpect(status().isCreated());
+        }
     }
 
-    @Test
-    void 회원가입실패_email이null임() throws Exception {
-        // given
-        final String url = "/api/v1/sign-up";
-        final SignUpRequest request = new SignUpRequest("name", null, "password");
+    @Nested
+    class 로그인테스트 {
 
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
+        @ParameterizedTest(name = "{1}")
+        @MethodSource("provideInvalidSignInRequests")
+        void 로그인실패_잘못된요청값(SignInRequest request, String description) throws Exception {
+            // given
+            final String url = "/api/v1/sign-in";
 
-        // then
-        resultActions.andExpect(status().isBadRequest());
-    }
+            // when
+            final ResultActions resultActions = mockMvc.perform(
+                    MockMvcRequestBuilders.post(url)
+                            .content(gson.toJson(request))
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
 
-    @Test
-    void 회원가입실패_password가null임() throws Exception {
-        // given
-        final String url = "/api/v1/sign-up";
-        final SignUpRequest request = new SignUpRequest("name", "name@email.com", null);
+            // then
+            resultActions.andExpect(status().isBadRequest());
+        }
 
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
+        private static Stream<Arguments> provideInvalidSignInRequests() {
+            return Stream.of(
+                    Arguments.of(new SignInRequest(null, "password"), "email이 없음"),
+                    Arguments.of(new SignInRequest("name@email.com", null), "password가 없음"),
+                    Arguments.of(new SignInRequest("wrong email form", "password"), "email 형식이 잘못됨")
+            );
+        }
 
-        // then
-        resultActions.andExpect(status().isBadRequest());
-    }
+        @Test
+        void 로그인실패_존재하지않는이메일() throws Exception {
+            // given
+            final String url = "/api/v1/sign-in";
+            final SignInRequest request = new SignInRequest("name@email.com", "password");
+            doThrow(new MemberException(MemberErrorResult.INVALID_ID_OR_PASSWORD)).when(signService).signIn(request);
 
-    @Test
-    void 회원가입실패_email형식이아님() throws Exception {
-        // given
-        final String url = "/api/v1/sign-up";
-        final SignUpRequest request = new SignUpRequest("name", "wrong email form", "password");
+            // when
+            final ResultActions resultActions = mockMvc.perform(
+                    MockMvcRequestBuilders.post(url)
+                            .content(gson.toJson(request))
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
 
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
+            // then
+            resultActions.andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value(MemberErrorResult.INVALID_ID_OR_PASSWORD.name()));
+        }
 
-        // then
-        resultActions.andExpect(status().isBadRequest());
-    }
+        @Test
+        void 로그인실패_틀린비밀번호() throws Exception {
+            // given
+            final String url = "/api/v1/sign-in";
+            final SignInRequest request = new SignInRequest("name@email.com", "wrong password");
+            doThrow(new MemberException(MemberErrorResult.INVALID_ID_OR_PASSWORD)).when(signService).signIn(request);
 
-    @Test
-    void 회원가입실패_이미등록된email임() throws Exception {
-        // given
-        final String url = "/api/v1/sign-up";
-        final SignUpRequest request = new SignUpRequest("name", "name@email.com", "password");
-        doThrow(new MemberException(MemberErrorResult.DUPLICATED_MEMBER_EMAIL)).when(signService).signUp(request);
+            // when
+            final ResultActions resultActions = mockMvc.perform(
+                    MockMvcRequestBuilders.post(url)
+                            .content(gson.toJson(request))
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
 
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-
-        // then
-        resultActions.andExpect(status().isBadRequest())
-                .andExpectAll(jsonPath("$.code").value("DUPLICATED_MEMBER_EMAIL"),
-                        jsonPath("$.message").value("이미 존재하는 이메일입니다."));
-    }
-
-    @Test
-    void 회원가입성공() throws Exception {
-        // given
-        final String url = "/api/v1/sign-up";
-        final SignUpRequest request = new SignUpRequest("name", "name@email.com", "password");
-
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-
-        // then
-        resultActions.andExpect(status().isCreated());
-    }
-
-    @Test
-    void 로그인실패_email이없음() throws Exception {
-        // given
-        final String url = "/api/v1/sign-in";
-        final SignInRequest request = new SignInRequest(null, "password");
-
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-
-        // then
-        resultActions.andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void 로그인실패_password가없음() throws Exception {
-        // given
-        final String url = "/api/v1/sign-in";
-        final SignInRequest request = new SignInRequest("name@email.com", null);
-
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-
-        // then
-        resultActions.andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void 로그인실패_틀린email형식임() throws Exception {
-        // given
-        final String url = "/api/v1/sign-in";
-        final SignInRequest request = new SignInRequest("wrong email form", "password");
-
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-
-        // then
-        resultActions.andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void 로그인실패_존재하지않는이메일() throws Exception {
-        // given
-        final String url = "/api/v1/sign-in";
-        final SignInRequest request = new SignInRequest("name@email.com", "password");
-        doThrow(new MemberException(MemberErrorResult.INVALID_ID_OR_PASSWORD)).when(signService).signIn(request);
-
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-
-        // then
-        resultActions.andExpect(status().isBadRequest())
-                .andExpectAll(jsonPath("$.code").value("INVALID_ID_OR_PASSWORD"),
-                        jsonPath("$.message").value("잘못된 이메일 또는 비밀번호입니다."));
-    }
-
-    @Test
-    void 로그인실패_틀린비밀번호() throws Exception {
-        // given
-        final String url = "/api/v1/sign-in";
-        final SignInRequest request = new SignInRequest("name@email.com", "wrong password");
-        doThrow(new MemberException(MemberErrorResult.INVALID_ID_OR_PASSWORD)).when(signService).signIn(request);
-
-        // when
-        final ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-
-        // then
-        resultActions.andExpect(status().isBadRequest())
-                .andExpectAll(jsonPath("$.code").value("INVALID_ID_OR_PASSWORD"),
-                        jsonPath("$.message").value("잘못된 이메일 또는 비밀번호입니다."));
+            // then
+            resultActions.andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value(MemberErrorResult.INVALID_ID_OR_PASSWORD.name()));
+        }
     }
 }
