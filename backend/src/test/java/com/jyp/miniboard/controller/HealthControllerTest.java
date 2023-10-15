b495d43694e52c7496553ae5e1739e5733a4b9f1
JJongmen <pjy90123@gmail.com>
2023-10-15 20:14:45 +0900

    HealthCheck용 API 구현

---
+++ backend/src/test/java/com/jyp/miniboard/controller/HealthControllerTest.java
@@ -0,0 +1,43 @@
+package com.jyp.miniboard.controller;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private Gson gson;
+
+    @Test
+    void mockMvc가null이아님() {
+        assertThat(mockMvc).isNotNull();
+        assertThat(gson).isNotNull();
+    }
+
+    @Test
+    void 서버헬스체크() throws Exception {
+        // given
+        final String url = "/";
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get(url)
+        );
+
+        // then
+        resultActions.andExpect(status().isOk());
+    }
+}
