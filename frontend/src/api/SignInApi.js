a443221a08ad85b9a2befe7162f19f4306e7588e
JJongmen <pjy90123@gmail.com>
2023-10-15 15:03:03 +0900

    배포한 서버로 API 요청하도록 수정

--- frontend/src/api/SignInApi.js
+++ frontend/src/api/SignInApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/v1';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const signIn = (email, password) => {
     const endpoint = `${BASE_URL}/sign-in`;
--- frontend/src/api/SignUpApi.js
+++ frontend/src/api/SignUpApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/v1';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const signUp = async (name, email, password) => {
   try {
