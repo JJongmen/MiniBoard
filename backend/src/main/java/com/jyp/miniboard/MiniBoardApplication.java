54e1fa8fd6a2d4ecbd0ba952e08a64042715acb2
JJongmen <pjy90123@gmail.com>
2023-10-18 20:13:56 +0900

    서버 타임존 UTC -> KST 변경

--- backend/src/main/java/com/jyp/miniboard/MiniBoardApplication.java
+++ backend/src/main/java/com/jyp/miniboard/MiniBoardApplication.java
@@ -1,10 +1,22 @@
 package com.jyp.miniboard;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MiniBoardApplication {
+
+    public static final String KST = "Asia/Seoul";
+
+    @PostConstruct
+    public void started() {
+        // timezone KST 설정
+        TimeZone.setDefault(TimeZone.getTimeZone(KST));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MiniBoardApplication.class, args);
